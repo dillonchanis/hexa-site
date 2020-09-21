b6f35299a983bb917db4248744944ccbd61b134d
Dillon Chanis <dillonchanis92@gmail.com>
2020-09-21 11:56:26 -0400

    Fixing sitename

--- gridsome.config.js
+++ gridsome.config.js
@@ -3,7 +3,7 @@ module.exports = {
   siteUrl: "https://www.tryhexa.design/",
   siteDescription:
     "A free color contrast tool to check for WCAG compliance for your desktop.",
-  titleTemplate: "<siteName> | %s",
+  titleTemplate: "Hexa | %s",
   plugins: [
     {
       use: "gridsome-plugin-tailwindcss",
